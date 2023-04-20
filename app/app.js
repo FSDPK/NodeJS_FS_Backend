26eb02272b27f6217de135f65e1f5187a633a250
sultanmehmood123@gmail.com <sultanmehmood123@gmail.com>
2023-04-20 12:01:58 +0500

    Router

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const userRouter = require("../router/userRouter");
 
 const app = express();
 // use middleware to form our contract for incoming json payloads ONLY!!
@@ -16,7 +17,7 @@ app.get("/", (req, res, next) => {
 });
 
 // routers
-// app.use("/register", registrationRouter)
+app.use("/users", userRouter);
 // bad url or error we can handle
 // with middleware
 app.use((req, res, next) => {
